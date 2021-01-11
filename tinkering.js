66d0a2eccfb44fd6ccc263cef91506832ec716cb
Anita Toth <haluska99@hotmail.com>
2021-01-11 20:17:10 +0000

    Adding from an earlier prep exercise so tree will be clean

---
+++ tinkering.js
@@ -0,0 +1,13 @@
+function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2,4);
+
+console.log(result);
+
+var bill = (10.25 + 3.99 + 7.15);
+var tip = bill*(.15);
+var total = "bill + tip";
+
+console.log(total);
